fc1f2b3fcbf65d0488d2f128e73fc6afaaec1fad
darnny-x <danielatbhi@gmail.com>
2026-03-05 03:01:42 +0000

    Update ProgramA.py

--- TASK 1 Computer Mathematics/ProgramA.py
+++ TASK 1 Computer Mathematics/ProgramA.py
@@ -49,9 +49,9 @@ def option1_lines(n: int) -> list[str]:
     
 def Convert(): # Convert (decimal → hex and binary)
     # Option 1 handler
-    n = input("Enter an integer (0..65535): ")
-    ans=option1_lines(n)
-    print(ans)
+    n = int(input("Enter an integer (0..65535): "))
+    for line in option1_lines(n):
+        print(line)
 
 def Littleendian(): # Little-endian pack/unpack (16-bit) + memory write/read
     pass
@@ -65,54 +65,42 @@ def ArrayAdressing(): # Array addressing + dereference (read/write one element)
 def StackFrame(): # Stack frame (simplified bp offsets) + register-style view
     pass
 
-
 def main():
     running = True
     while running:
-            # print the menu
-            print('Choose the operation:')
-            print(' 1- Convert (decimal → hex and binary)')
-            print(' 2- Little-endian pack/unpack (16-bit) + memory write/read')
-            print(' 3- ASCII memory dump + null terminator + length scan')
-            print(' 4- Array addressing + dereference (read/write one element)')
-            print(' 5- Stack frame (simplified bp offsets) + register-style view')
-            print(' 6- Quit')
-        
+        # print the menu
+        print("Choose the operation:")
+        print(" 1- Convert (decimal → hex and binary)")
+        print(" 2- Little-endian pack/unpack (16-bit) + memory write/read")
+        print(" 3- ASCII memory dump + null terminator + length scan")
+        print(" 4- Array addressing + dereference (read/write one element)")
+        print(" 5- Stack frame (simplified bp offsets) + register-style view")
+        print(" 0- Quit")
 
-        # get the option
-            op = input('Option: ')
+         # get the option
+        op = input("Option: ")
 
-            if op == '1':
+        if op == "1":
             # 1 -  Convert (decimal → hex and binary)
-                Convert()
-         
-
-            elif op == '2':
-            # 2 - Little-endian pack/unpack (16-bit) + memory write/read
-                Littleendian()
-        
-            elif op == '3':
+            Convert()
+        elif op == "2":
+             # 2 - Little-endian pack/unpack (16-bit) + memory write/read
+            Littleendian()
+        elif op == "3":
             # 3 - ASCII memory dump + null terminator + length scan
-                ASCII()
-
-            elif op == '4':
+            ASCII()
+        elif op == "4":
             # 4 - Array addressing + dereference (read/write one element)
-                ArrayAdressing()
-
-            elif op == '5':
-            # 5 - Stack frame (simplified bp offsets) + register-style view
-                StackFrame()
-            
-            elif op == '6':
-                running = False
-            # Quits the program
-            
-            else:
-            # the user did not enter an option that exists in the menu
-                print('Invalid option. Try again')
+            ArrayAdressing()
+        elif op == "5":
+             # 5 - Stack frame (simplified bp offsets) + register-style view
+            StackFrame()
+        elif op == "0":
+             # Quits the program
+            running = False
+        else:
+             # the user did not enter an option that exists in the menu
+            print("Invalid option. Try again")
 
 if __name__ == '__main__':
     main()
-
-
-
