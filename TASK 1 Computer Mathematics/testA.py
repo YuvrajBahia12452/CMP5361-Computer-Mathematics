622cef7be6f13b70082ae0f2ad830b8ef982a5a0
darnny-x <danielatbhi@gmail.com>
2026-03-05 02:11:03 +0000

    Update testA.py

--- TASK 1 Computer Mathematics/testA.py
+++ TASK 1 Computer Mathematics/testA.py
@@ -1,6 +1,34 @@
 import unittest
-from ProgramA import Convert, Littleendian, ArrayAdressing, ASCII, StackFrame
+import ProgramA
+
+class TestOption1(unittest.TestCase):
+
+    def test_hexu16(self):
+        self.assertEqual(ProgramA.hex_u16(10), "000A")
+        self.assertEqual(ProgramA.hex_u16(65535), "FFFF")
+
+    def test_bin16_is_16_bits(self):
+        self.assertEqual(ProgramA.bin16(5), "0000000000000101")
+        self.assertEqual(len(ProgramA.bin16(5)), 16)
+
+    def test_signed16_positive_range(self):
+        self.assertEqual(ProgramA.signed16(0), 0)
+        self.assertEqual(ProgramA.signed16(32767), 32767)
+
+    def test_signed16_negative_range(self):
+        self.assertEqual(ProgramA.signed16(32768), -32768)
+        self.assertEqual(ProgramA.signed16(65535), -1)
+
+    def test_require_u16_rejects_out_of_range(self):
+        with self.assertRaises(ValueError):
+            ProgramA.require_u16(-1)
+        with self.assertRaises(ValueError):
+            ProgramA.require_u16(65536)
+
+    def test_require_u16_rejects_non_int(self):
+        with self.assertRaises(TypeError):
+            ProgramA.require_u16("123")  # string not allowed
 
 
-
-
+if __name__ == "__main__":
+    unittest.main()
