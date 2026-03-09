ee76788dfb13556859b368d17999a508df9c0315
SUKHMAN1424 <sukhmanjotkaur.-@mail.bcu.ac.uk>
2026-03-09 16:21:36 +0000

    Update option5_stack_frame.py

--- option5_stack_frame.py
+++ option5_stack_frame.py
@@ -1,10 +1,10 @@
 # Simple simulation of a stack frame using bp offsets
 
 # asking the user for two numbers
-a = int(input("Enter value for a: "))
-b = int(input("Enter value for b: "))
+a = int(input("Input number a: "))
+b = int(input("Input number b: "))
 
-print("\nStack Frame View")
+print("\nSimulated Stack Frame")
 print("----------------")
 print("bp    : RETURN address")
 print("bp+2  : a =", a)
